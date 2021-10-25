1abcfeb5358299e1ce0ddc0eb5df857c3147b38a
Ahmed Aly <aaoflaca@gmail.com>
2021-10-24 19:42:45 -0700

    commit 10242021-01

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
+  const history = useHistory();
+
+  const handleAddToCart = () => {
+    history.push(`/cart/${product._id}?qty=1`);
+  };
   return (
     <Card className="my-2 p-2 rounded">
       <Link to={`/product/${product._id}`}>
@@ -22,6 +27,25 @@ const Product = ({ product }) => {
             text={`${product.numReviews} reviews`}
           />
         </Card.Text>
+        <Card.Text as="span">
+          <Row style={{ alignItems: "center" }}>
+            <Col xs={9}>
+              {product.countInStock > 0
+                ? `${product.countInStock} Items In Stock`
+                : "Out Of Stock"}
+            </Col>
+            {product.countInStock > 0 && (
+              <Col xs={3} style={{ alignItems: "center" }}>
+                <Button type="button" variant="light" onClick={handleAddToCart}>
+                  {" "}
+                  {/* Add */}
+                  <i className="fa-solid fa-cart-plus"></i>
+                </Button>{" "}
+              </Col>
+            )}
+          </Row>
+        </Card.Text>
+
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
